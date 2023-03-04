116fd7d8dbc9f4b318fd7559565132e7d44519a3
AnushkaM02 <anushka.mulik@gmail.com>
2023-03-04 16:10:15 +0530

    pages structure

---
+++ client/blind-dating-app/src/App.js
@@ -0,0 +1,20 @@
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import OnBoarding from "./pages/OnBoarding";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* {authToken &&  */}
+        <Route path="/dashboard" element={<Dashboard />} />}
+        {/* {authToken &&  */}
+        <Route path="/onboarding" element={<OnBoarding />} />}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
